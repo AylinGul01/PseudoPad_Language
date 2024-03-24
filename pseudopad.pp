50559ea9e9b646f64f1eb2786be7d5a0bffcc56b
Nigarselekk <nigarselek923@gmail.com>
2024-03-24 23:19:30 +0300

    example file has added

---
+++ pseudopad.pp
@@ -0,0 +1,18 @@
+algorithm TestProgram
+    input: x, y
+    output: sum
+
+    // Variable declarations
+    integer x, y, sum
+
+    // Reading input values
+    print "Enter two integers:"
+    scan x, y
+   
+
+    //Arithmetic operation 
+    sum = x plus y
+
+    // Output results
+    print "Sum:", sum
+ end algorithm
